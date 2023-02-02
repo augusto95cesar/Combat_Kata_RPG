c02a5cc6ac8ca1cb0053766d35a05402bdaffe3a
Augusto Cesar <augusto95cesar@gmail.com>
2023-02-02 03:03:09 -0300

    - Add class Personagem

---
+++ KataRPG/KataModel/Entity/Personagem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KataModel.Entity
+{
+    public class Personagem
+    {
+        private Guid Id;
+        public Personagem()
+        {
+            Id = Guid.NewGuid(); 
+            Saude = 1000;
+            Nivel = 1;
+            Vivo = true;
+        }
+        public double Saude { get; set; }
+        public int Nivel { get; set; }
+        public bool Vivo { get; set; }       
+        public Guid GetId()
+        {
+            return Id;
+        }     
+    }
+}
---
+++ KataRPG/KataTest/IterationOneTest.cs
@@ -0,0 +1,129 @@
+﻿using KataModel.Entity;
+using KataModel.Services;
+using NUnit.Framework;
+
+namespace KataTest
+{
+    [TestFixture]
+    public class IterationOneTest
+    {
+       private Personagem _protagonista;
+       private Personagem _inimigo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _protagonista = PersonagemService.CriarPersonagem();
+            _inimigo = PersonagemService.CriarPersonagem();
+        }
+
+        /// <summary>
+        /// Health, starting at 1000
+        /// </summary>
+        [Test]
+        public void SaudeMil()
+        {
+            Assert.AreEqual(_protagonista.Saude, 1000);
+            Assert.AreEqual(_inimigo.Saude, 1000);
+        }
+
+        /// <summary>
+        /// Level, starting at 1
+        /// </summary>
+        [Test]
+        public void NivelUm()
+        {
+            Assert.AreEqual(_protagonista.Nivel, 1);
+            Assert.AreEqual(_inimigo.Nivel, 1);
+        }
+
+        /// <summary>
+        ///  May be Alive or Dead, starting Alive (Alive may be a true/false)
+        /// </summary>
+        [Test]
+        public void EstãoVivo()
+        {
+            Assert.AreEqual(_protagonista.Vivo, true);
+            Assert.AreEqual(_inimigo.Vivo, true);
+        }
+
+        /// <summary>
+        /// Damage is subtracted from Health
+        /// </summary>
+        [Test]
+        public void Ataque()
+        {
+            var ataqueInimigo = 200;
+            _protagonista.Dano(_inimigo, ataqueInimigo);
+
+            var ataque = (_inimigo.Nivel - _protagonista.Nivel) >= 5 ? ataqueInimigo / 2 : (ataqueInimigo * 1.5);
+
+            Assert.AreEqual(_protagonista.Saude, 1000 - ataque);
+
+        }
+
+        /// <summary>
+        /// When damage received exceeds current Health, Health becomes 0 and the character dies
+        /// </summary>
+        [Test]
+        public void AtaqueExcedeASaude()
+        {
+            var ataqueInimigo = 900;
+            _protagonista.Dano(_inimigo, ataqueInimigo); 
+
+            Assert.AreEqual(_protagonista.Saude, 0);
+            Assert.AreEqual(_protagonista.Vivo, false);
+        }
+
+        /// <summary>
+        /// Dead characters cannot be healed
+        /// </summary>
+        [Test]
+        public void CurarPersonagemMorto()
+        {
+            var ataqueInimigo = 900;
+            _protagonista.Dano(_inimigo, ataqueInimigo);
+
+            _protagonista.Curar(500);
+
+            Assert.AreEqual(_protagonista.Saude,0);
+            Assert.AreEqual(_protagonista.Vivo, false);
+        }
+
+        /// <summary>
+        /// Healing cannot raise health above 1000
+
+        /// </summary>
+        [Test]
+        public void CurarAcimaDeMil()
+        {           
+            _protagonista.Curar(500);
+
+            Assert.AreEqual(_protagonista.Saude, 1000);
+            Assert.AreEqual(_protagonista.Vivo, true);
+        }
+
+        /// <summary>
+        /// Cura 500
+        /// </summary>
+
+        [Test]
+        public void CuraQuinhentos()
+        {
+            var ataqueInimigo = 500;
+            _protagonista.Dano(_inimigo, ataqueInimigo);
+
+            _protagonista.Curar(700);
+ 
+            Assert.AreEqual(_protagonista.Saude, 950);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _protagonista = null;
+            _inimigo = null;
+        }
+    }
+}
